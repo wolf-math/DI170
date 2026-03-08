1e27c8f6c6ce1f232fdaf517c2154327fe30d42e
Aaron Wolf <aaron.wolf@gmail.com>
2026-03-08 18:52:46 +0200

    week12

--- week11/day5/async.js
+++ week11/day5/async.js
@@ -1,22 +1,69 @@
-// Don't do this
-
 async function hello() {
   return 'Hello';
 }
+
+// Don't do this
 
 const b = hello();
 
 b.then((result) => console.log(result));
 
 // Do this!!!
-
-async function hello() {
-  return 'Hello';
-}
-
 async function sayHi() {
   const result = await hello();
   console.log(result);
 }
 
 sayHi();
+
+// ==================
+
+// let goodGrades = 93;
+
+// let endSemester = new Promise((resolve, reject) => {
+//   if (goodGrades > 90) {
+//     resolve('Computer');
+//   } else if ((goodGrades) => 80 && goodGrades <= 89) {
+//     resolve('Phone');
+//   } else {
+//     reject("I won't get the gift");
+//   }
+// });
+
+// const checkRequest = () => {
+//   console.log('test 2');
+//   endSemester.then((value) => console.log('I got an amazing gift : A ', value));
+// };
+
+// console.log('test 1');
+// checkRequest();
+// console.log('test 3');
+
+
+
+let goodGrades = 93;
+
+const endSemester = new Promise((resolve, reject) => {
+  if (goodGrades > 90) {
+    resolve('Computer');
+  } else if (goodGrades >= 80 && goodGrades <= 89) {
+    // fixed comparison
+    resolve('Phone');
+  } else {
+    reject("I won't get the gift");
+  }
+});
+
+async function checkRequest() {
+  console.log('test 2');
+  try {
+    const gift = await endSemester;
+    console.log('I got an amazing gift: A', gift);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log('test 1');
+checkRequest();
+console.log('test 3');
---
+++ week12/pokemon/script.js
@@ -0,0 +1,15 @@
+function createRandomNum() {}
+function getPokemon(id) {}
+
+let id = createRandomNum();
+
+// get a random pokemon
+getPokemon(id);
+
+// get next pokemon
+++id;
+getPokemon(id);
+
+// get prev pokemon
+--id;
+getPokemon(id);
